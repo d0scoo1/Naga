4eef707b8e7338c7851ba366d8ccf6675d13e31b
d0scoo1 <victory_yan@foxmail.com>
2022-07-12 19:37:36 +0800

    Timeout

--- tests/naga_test.py
+++ tests/naga_test.py
@@ -4,6 +4,10 @@ from slither_compiler import *
 import os
 import time
 from naga.naga import Naga
+from func_timeout.exceptions import FunctionTimedOut
+from func_timeout import func_set_timeout
+
+time_out_seconds = 10
 
 class NagaTest():
     def __init__(self,contract,input_dir,output_dir) -> None:
@@ -13,39 +17,56 @@ class NagaTest():
         self.contracts_dir = os.path.join(input_dir,'contracts')
         self.result_path = os.path.join(output_dir,'results')
 
-    def get_slither(self):
+    def _get_slither(self):
         sol_dir = os.path.join(self.contracts_dir, self.contract['address'] + '_' + self.contract['name'])
         if os.path.exists(sol_dir):
+            #print(sol_dir,self.contract['compiler'])
             return multi_compile(sol_dir,self.contract['compiler'])
         sol_file = sol_dir +'.sol'
         if os.path.exists(sol_file):
+            #print(sol_file,self.contract['compiler'])
             return single_compile(sol_file,self.contract['compiler'])
-        
-        
-        
-        #return etherscan_download_compile(self.contract['address'],self.contract['name'],self.contract['compiler'],contracts_dir)
-        
-    def local_test(self):
 
+        #return etherscan_download_compile(self.contract['address'],self.contract['name'],self.contract['compiler'],contracts_dir)
+
+    def local_test(self):
         try:
             s_start_time = time.time()
-            slither = self.get_slither()
+            slither = self._get_slither()
             s_end_time = time.time()
             self.contract['slither_compile_cost'] = s_end_time - s_start_time
+        except FunctionTimedOut:
+            self._write_error('slither_timeout')
+            return
         except:
-            with open(os.path.join(self.output_dir,'errors',self.contract['address']+'_slither'),'w') as f:
-                pass
+            self._write_error('slither_compileError')
+            return
+        
+        try:
+            naga_contract = self._naga_test(slither)
+        except FunctionTimedOut:
+            self._write_error('naga_timeout')
+            return
+        except:
+            self._write_error('naga_error')
             return
 
+        return naga_contract
+
+    @func_set_timeout(time_out_seconds)
+    def _naga_test(self,slither):
         n_start_time = time.time()
         naga = Naga(slither,contract_name= self.contract['name'])
-        if len(naga.entry_contracts) == 0: return
+        if len(naga.entry_contracts) == 0: 
+            self._write_error('naga_noEntryContract')
+            return
         naga_contract = naga.entry_contracts[0]
         if self.contract['erc_force'] != None:
             naga_contract.detect(erc_force= self.contract['erc_force'])
         elif naga_contract.is_erc:
             naga_contract.detect()
         else:
+            self._write_error('naga_isNotERC')
             return
         n_end_time = time.time()
         self.contract['naga_test_cost'] = n_end_time - n_start_time
@@ -53,7 +74,17 @@ class NagaTest():
 
         naga_contract.set_info(self.contract)
         naga_contract.summary_json(os.path.join(self.result_path,self.contract['address']))
+        
         return naga_contract
+
+
+    def _write_error(self,error_msg):
+        '''
+        naga_errors: noEntryContract,timeout,isNotERC,error
+        slither_errors: timeout,compileError
+        '''
+        with open(os.path.join(self.output_dir,'errors',self.contract['address']+'_'+error_msg),'w') as f:
+                pass
 
 from multiprocessing import Process,Queue
 from tqdm import tqdm
@@ -68,11 +99,9 @@ def consumer(q,input_dir,output_dir):
         c = q.get()
         if c:
             nagaT = NagaTest(c,input_dir,output_dir)
-            try:
-                nagaT.local_test()
-            except:
-                with open(os.path.join(output_dir,'errors',c['address']+'_naga'),'w') as f:
-                    pass
+            
+            nagaT.local_test()
+            
         else:
             return
 
@@ -90,9 +119,13 @@ def _load_contracts(input_dir,output_dir,erc_force = None):
             contracts_info.append(json.loads(line))
             line = fr.readline()
 
+    staled_contracts = []
     contracts = []
     for c in contracts_info:
-        if c['address'] in contracts_tested or int(c['compiler'][2]) <= 4:
+        if int(c['compiler'][2]) <= 4:
+            staled_contracts.append(c)
+            continue
+        if c['address'] in contracts_tested:
             continue
         contractInfo ={
             'address': c['address'],
@@ -106,16 +139,42 @@ def _load_contracts(input_dir,output_dir,erc_force = None):
             'implementation':c['Implementation'],
         }
         contracts.append(contractInfo)
+    
+    print('contracts_info:',len(contracts_info))
+    print('staled_contracts:',len(staled_contracts))
+
     return contracts
 
 def _count_errors(output_dir):
-    errors = []
-    slither_errors = []
+    naga_errors = []
+    naga_not_erc = []
+    naga_no_entry = []
+    naga_timeout = []
+    
+    slither_timeout = []
+    slither_compileError = []
     for c in os.listdir(os.path.join(output_dir,'errors')):
-        if c[43:] == 'naga': errors.append(c[:42])
-        else: slither_errors.append(c[:42])
-    print('naga errors:',len(errors))
-    print('slither errors:',len(slither_errors))
+        if c[43:].startswith('naga'): 
+            if c[43:].endswith('isNotERC'):
+                naga_not_erc.append(c[:42])
+            elif c[43:].endswith('noEntryContract'):
+                naga_no_entry.append(c[:42])
+            elif c[43:].endswith('timeout'):
+                naga_timeout.append(c[:42])
+            else:
+                naga_errors.append(c[:42])
+        else: 
+            if c[43:].endswith('timeout'):
+                slither_timeout.append(c[:42])
+            else:
+                slither_compileError.append(c[:42])
+
+    print('naga_errors:',len(naga_errors))
+    print('naga_not_erc:',len(naga_not_erc))
+    print('naga_no_entry:',len(naga_no_entry))
+    print('naga_timeout:',len(naga_timeout))
+    print('slither_timeout:',len(slither_timeout))
+    print('slither_compileError:',len(slither_compileError))
 
 def run(input_dir,output_dir, erc_force = None, process_num = 50):
     print('input_dir:',input_dir)
@@ -138,6 +197,7 @@ def run(input_dir,output_dir, erc_force = None, process_num = 50):
 
     print('naga results:', len(os.listdir(naga_results_dir)))
     _count_errors(output_dir)
+
 
 input_dir = '/home/yankailun/naga_test'
 output_dir = '/data/disk_16t_2/kailun/scc/naga_output'
--- tests/slither_compiler.py
+++ tests/slither_compiler.py
@@ -6,6 +6,7 @@ from test_platform.etherscan import Etherscan # crytic_compile.platform.Ethersca
 
 import logging
 logging.getLogger("CryticCompile").level = logging.CRITICAL
+from func_timeout import func_set_timeout
 
 ###### common config ######
 etherscan_api_key= '68I2GBGUU79X6YSIMA8KVGIMYSKTS6UDPI'
@@ -27,7 +28,9 @@ def get_solc_remaps(version='0.8.0',openzeppelin_dir = openzeppelin_dir):
         return "@openzeppelin/=" + openzeppelin_dir + "/openzeppelin-contracts-solc-0.8/"
     return "@openzeppelin/=" + openzeppelin_dir + "/openzeppelin-contracts-solc-0.8/"
 
+time_out_seconds = 10
 
+@func_set_timeout(time_out_seconds)
 def single_compile(sol_path,compiler_version):
     return Slither(sol_path,
         solc = get_solc(compiler_version),
@@ -35,6 +38,7 @@ def single_compile(sol_path,compiler_version):
         solc_remaps = get_solc_remaps(compiler_version)
     )
 
+@func_set_timeout(time_out_seconds)
 def multi_compile(sol_dir,compiler_version):
     return Slither(CryticCompile
                     (MultiSolFiles(
@@ -43,6 +47,7 @@ def multi_compile(sol_dir,compiler_version):
                     compiler_version=compiler_version),
                 disable_solc_warnings = True)
 
+@func_set_timeout(time_out_seconds)
 def etherscan_download_compile(contract_address,compiler_version,contracts_dir):
     return Slither(
         CryticCompile(
@@ -54,3 +59,16 @@ def etherscan_download_compile(contract_address,compiler_version,contracts_dir):
 
 def get_solc(compiler_version):
     return solc_dir +'solc-'+ compiler_version
+
+from func_timeout.exceptions import FunctionTimedOut
+if __name__ == "__main__":
+    #/home/yankailun/naga_test/token_tracker/erc1155/contracts/0x6737771b9e3d64b482b76e274ba77a612b564863_ContributionBoard
+    try:
+        slither = multi_compile('/home/yankailun/naga_test/token_tracker/erc1155/contracts/0x6737771b9e3d64b482b76e274ba77a612b564863_ContributionBoard','0.7.6')
+    except FunctionTimedOut:
+        print('timeout')
+
+    #slither = multi_compile('/home/yankailun/naga_test/mainnet/contracts/0x1273e2854f716c39f8657b9e41846e3acf253a76_MercedesBenz','0.8.7')
+    #print(slither)
+    #for c in slither.contracts:
+    #    print(c.name)
